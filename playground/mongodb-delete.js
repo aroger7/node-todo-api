1823cc6e375953eccaa014907a48b60d2b402c7e
Adam Rogers <cldd5678@hotmail.com>
2018-07-17 19:40:03 -0400

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,46 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db
+  //   .collection('Todos')
+  //   .deleteMany({ text: 'Eat lunch' })
+  //   .then(result => {
+  //     console.log(result);
+  //   });
+
+  // db
+  //   .collection('Todos')
+  //   .deleteOne({ text: 'Eat lunch' })
+  //   .then(result => {
+  //     console.log(result);
+  //   });
+  // db
+  //   .collection('Todos')
+  //   .findOneAndDelete({ completed: false })
+  //   .then(result => {
+  //     console.log(result);
+  //   });
+
+  db
+    .collection('Users')
+    .deleteMany({ name: 'Adam' })
+    .then(result => {
+      console.log(result);
+    });
+
+  // db
+  //   .collection('Users')
+  //   .findOneAndDelete({ _id: new ObjectID('5b4d337d02bd6a2330fd3829') })
+  //   .then(result => {
+  //     console.log(result);
+  //   });
+
+  //client.close();
+});
