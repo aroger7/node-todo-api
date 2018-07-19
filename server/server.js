ca825066dffab6f41722a854c0bb7ad52c62d818
Adam Rogers <cldd5678@hotmail.com>
2018-07-19 18:35:27 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -31,6 +32,23 @@ app.get('/todos', (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (todo) {
+        res.send({ todo });
+      } else {
+        res.status(404).send();
+      }
+    })
+    .catch(err => res.status(400).send());
 });
 
 app.listen(3000, () => {
