a27f983fad63e525fa73bd813bbdd0603794ca52
Adam Rogers <cldd5678@hotmail.com>
2018-07-21 18:29:30 -0400

    Add authentication logging

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -2,13 +2,16 @@ const { User } = require('./../models/user');
 
 const authenticate = (req, res, next) => {
   const token = req.header('x-auth');
+  console.log(`Attempting to authenticate token ${token}`);
 
   User.findByToken(token)
     .then(user => {
       if (!user) {
+        console.log(`Unable to authenticate token ${token}`);
         return Promise.reject();
       }
 
+      console.log(`Authenticated token as user ${user._id}, ${user.email}`);
       req.user = user;
       req.token = token;
       next();
--- server/models/user.js
+++ server/models/user.js
@@ -66,6 +66,7 @@ UserSchema.statics.findByToken = function(token) {
   try {
     decoded = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
+    console.log('Unable to decode token', e);
     return Promise.reject();
   }
 
